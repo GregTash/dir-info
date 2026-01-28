3fdbc2fb31279de865867cbeeda9c4a483e6acd1
Gregory Tashjian <gregtash4@gmail.com>
2026-01-29 01:45:45 +1100

    Fix memory leaks.

--- src/main.c
+++ src/main.c
@@ -31,10 +31,11 @@ int main(int argc, char* argv[]) {
 			return 1;
 		}
 
-		if (!opendir(argv[1])) {
+		if (!(dir = opendir(argv[1]))) {
 			printf("Directory %s does not exist!\n", argv[1]);
 			return 1;
 		}
+		closedir(dir);
 
 		strcpy(location, argv[1]); 
 	}
@@ -51,6 +52,7 @@ int main(int argc, char* argv[]) {
 		int len = strlen(ent->d_name);
 		char* fileEnd = &ent->d_name[len - strlen(fType)];
 		
+		//If the end of the file name matches the file type then do stuff
 		if (!strcmp(fileEnd, fType)) {
 			char* fileToOpen = malloc(sizeof(location) + strlen(ent->d_name) + 1);
 			strcpy(fileToOpen, location);
@@ -58,12 +60,14 @@ int main(int argc, char* argv[]) {
 			strcat(fileToOpen, ent->d_name);
 
 			FILE* file = fopen(fileToOpen, "r");
+			free(fileToOpen);
 
 			if (!file) {
 				printf("Could not open file: %s\n", ent->d_name);
 				return 1;
 			}
 
+			//Read through the file and analyse info
 			while (!feof(file)) {
 				int c = fgetc(file);
 
@@ -75,6 +79,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	printf("%lu\n", lineCount);
+	closedir(dir);
 
 	return 0;
 }
