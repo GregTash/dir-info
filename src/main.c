ebd9fbcef39bef5f9198c07e28afab7ca9c23dd9
Gregory Tashjian <gregtash4@gmail.com>
2026-01-29 17:07:12 +1100

    Add ability to recursively check a folder.

--- src/main.c
+++ src/main.c
@@ -2,18 +2,72 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #define DIR_MAX_LENGTH 4096
+char location[DIR_MAX_LENGTH];
+char* fType = NULL;
 
-int main(int argc, char* argv[]) {
-	char location[DIR_MAX_LENGTH];
+unsigned long lineCount = 0;
+
+int analyseDir(char* location, bool recursive) {
 	struct dirent* ent;
 	DIR* dir;
-	char* fType = NULL;
 
-	unsigned long lineCount = 0;
+	if (!(dir = opendir(location))) {
+		perror("Could not open directory!\n");
+		return 1;
+	}
 
+	while ((ent = readdir(dir))) {
+		int len = strlen(ent->d_name);
+		char* fileEnd = &ent->d_name[len - strlen(fType)];
+		
+		char* fileToOpen = malloc(strlen(location) + strlen(ent->d_name) + 2);
+		strcpy(fileToOpen, location);
+		strcat(fileToOpen, "/");
+		strcat(fileToOpen, ent->d_name);
+
+		struct stat pathStat;
+		stat(fileToOpen, &pathStat);
+		int isFile = S_ISREG(pathStat.st_mode);
+		if (!isFile && fileToOpen[strlen(fileToOpen)-1] != '.' && recursive) {
+			analyseDir(fileToOpen, true);
+
+			free(fileToOpen);
+			continue;
+		}
+
+		//If the end of the file name matches the file type then do stuff
+		if (!strcmp(fileEnd, fType)) {
+			FILE* file = fopen(fileToOpen, "r");
+
+			if (!file) {
+				printf("Could not open file: %s\n", ent->d_name);
+				return 0;
+			}
+
+			//Read through the file and analyse info
+			while (!feof(file)) {
+				int c = fgetc(file);
+
+				if (c == '\n') lineCount++;
+			}
+
+			fclose(file);
+		}
+
+		free(fileToOpen);
+	}
+
+	closedir(dir);
+	return 1;
+}
+
+int main(int argc, char* argv[]) {
+	DIR* dir;
 	//Get the directory location
 	if (argc < 1) {
 		printf("No file type specified!\n");
@@ -40,46 +94,16 @@ int main(int argc, char* argv[]) {
 		strcpy(location, argv[1]); 
 	}
 
-	if (!(dir = opendir(location))) {
-		perror("Could not open directory!\n");
-		return 1;
-	}
 
 	fType = argv[argc-1];
 
 	//Analyse the directory
-	while ((ent = readdir(dir))) {
-		int len = strlen(ent->d_name);
-		char* fileEnd = &ent->d_name[len - strlen(fType)];
-		
-		//If the end of the file name matches the file type then do stuff
-		if (!strcmp(fileEnd, fType)) {
-			char* fileToOpen = malloc(sizeof(location) + strlen(ent->d_name) + 1);
-			strcpy(fileToOpen, location);
-			strcat(fileToOpen, "/");
-			strcat(fileToOpen, ent->d_name);
-
-			FILE* file = fopen(fileToOpen, "r");
-			free(fileToOpen);
-
-			if (!file) {
-				printf("Could not open file: %s\n", ent->d_name);
-				return 1;
-			}
-
-			//Read through the file and analyse info
-			while (!feof(file)) {
-				int c = fgetc(file);
-
-				if (c == '\n') lineCount++;
-			}
-
-			fclose(file);
-		}
+	if (!analyseDir(location, true)) {
+		printf("Analysing directory failed!\n");
+		return 1;
 	}
 
-	printf("%lu\n", lineCount);
-	closedir(dir);
+	printf("Lines: %lu\n", lineCount);
 
 	return 0;
 }
